c7ab092fcbceb827d33942850fcc524ad21a7084
Annie Won <63106227+Anniejh0811@users.noreply.github.com>
2022-01-04 16:07:45 -0800

    Update RestaurantReservationApplication.java

--- RestaurantReservationApplication.java
+++ RestaurantReservationApplication.java
@@ -1,8 +1,3 @@
-// --== CS400 File Header Information ==--
-// Name: Annie Won
-// Email: jwon23@wisc.edu
-// Notes to Grader: None
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
